456317ecc1bbbcb9f7ebc1a4612e457c6db5cdf3
Sinan Ur Rehman <111499190+Sinanurrehman@users.noreply.github.com>
2024-07-25 10:02:51 +0500

    day5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:application_1/widhget/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:application_1/pages/home_page.dart';
 import 'package:application_1/pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
